729854ea603ae9ce6caa6d02ea3b6764adeb461f
Marcel-NG <uduma.matrix@gmail.com>
2023-02-05 03:01:44 +0100

    I am still trying to figure out a couple of things out in this commit, like how to just to get a balanceresponse.

--- main.go
+++ main.go
@@ -9,11 +9,34 @@ import (
 * After all said and done more is actual said than done
  */
 
+//  type balance struct {
+// 	Name string
+// 	amount float32
+//  }
+
 func main() {
 	fmt.Println("Go has started from here now")
 
+	// Balance := balance {
+	// 	Name: ,
+	// }
+
+	var BALANCE = map[string]interface{}{
+		"marcel": 100000,
+		"chidi":  4000,
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Welcome to my website!")
+	})
+
+	http.HandleFunc("/balance", func(w http.ResponseWriter, r *http.Request) {
+
+		// I am going to be printing the outputs of the users balance in the parameters here.
+		user := r.URL.Query().Get("user")
+		balance := BALANCE[user]
+		fmt.Println("The balance is: \t", balance)
+		fmt.Fprintf(w, "We are trying to get the balance")
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
